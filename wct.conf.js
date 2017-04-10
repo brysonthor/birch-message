2208580799f834afb2cf19b96243544b2c0b2c5b
Chad Eddington <cdavideddington@gmail.com>
2017-04-10 14:03:02 -0600

    Set up Travis (#4)

--- wct.conf.js
+++ wct.conf.js
@@ -1,18 +1,12 @@
 module.exports = {
   "plugins": {
-    "istanbul": {
-      "dir": "./coverage",
-      "reporters": [
-        "text-summary", "lcov"
-      ],
-      "exclude": [],
-
-      "include": [
-        "**/birch-message.html"
-      ]
-    },
-    "local": {
-      "browsers": ["chrome", "safari"]
+    "sauce": {
+      "disabled": true, 
+      "browsers": [
+        "Windows 8.1/internet explorer",
+        "Windows 10/microsoftedge",
+        "OS X 10.10/safari"
+    ]
     }
   }
 }
